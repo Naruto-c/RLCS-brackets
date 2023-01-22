9385fd2fb504eeb5e245d3efa426679022aaa9c3
Ian Lai <Leadmlg.wolfy@gmail.com>
2023-01-22 12:33:31 -0800

    Changed the driver to chrome
    Hopefully will access the data from the Octane.gg. Current code does launch the website in chrome instead of my default browser

--- Java/AccessData/Data.py
+++ Java/AccessData/Data.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # Accessing octane.gg's stats on all the RLCS teams
-driver = webdriver.Opera(
-    "https://octane.gg/stats/teams?mode=3&minGames=50&group=rlcs2122fall")
+driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 
 winPercentage = []  # Win percentage of each team
 goalsPerGame = []  # Average goals per game of each team
@@ -12,3 +11,4 @@ assistsPerGame = []  # Average assists per game of each team
 savesPerGame = []  # Average saves per game of each team
 shotsPerGame = []  # Average shots per game of each team
 shootingPercentage = []  # Shooting percentrage of each team
+driver.get("https://octane.gg/stats/teams?mode=3&minGames=50&group=rlcs2122fall")
