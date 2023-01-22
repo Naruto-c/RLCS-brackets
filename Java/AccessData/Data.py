43fb5439d7f309f8001f92a1f07ccb8e0c182f98
Ian Lai <Leadmlg.wolfy@gmail.com>
2023-01-22 12:27:24 -0800

    Added arrays to keep track of all the data

--- Java/AccessData/Data.py
+++ Java/AccessData/Data.py
@@ -1,3 +1,14 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+
+# Accessing octane.gg's stats on all the RLCS teams
+driver = webdriver.Opera(
+    "https://octane.gg/stats/teams?mode=3&minGames=50&group=rlcs2122fall")
+
+winPercentage = []  # Win percentage of each team
+goalsPerGame = []  # Average goals per game of each team
+assistsPerGame = []  # Average assists per game of each team
+savesPerGame = []  # Average saves per game of each team
+shotsPerGame = []  # Average shots per game of each team
+shootingPercentage = []  # Shooting percentrage of each team
