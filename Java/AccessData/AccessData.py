02f68c66958ee2c6cbc9663264a434ec971f2ac5
Ian lai <Leadmlg.wolfy@gmail.com>
2023-01-23 09:45:50 -0800

    Revert "Access the website and prints the html"
    This reverts commit 7653ce55c56edd05c601fd5c874442d6360dc54f.

--- Java/AccessData/AccessData.py
+++ Java/AccessData/AccessData.py
@@ -2,11 +2,3 @@ import pandas as pd
 import re
 import requests
 from bs4 import BeautifulSoup
-
-url = "https://octane.gg/stats/teams?mode=3&region=NA&region=EU&region=OCE&region=SAM&region=ASIA&region=ME&region=AF&region=INT&minGames=250"
-
-page = requests.get(url)
-soup = BeautifulSoup(page.text)
-
-row = soup.find('tr')
-print(soup)
