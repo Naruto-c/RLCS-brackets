98ad5236c815c045219664e3dcab621ada3e7ed3
Ian lai <Leadmlg.wolfy@gmail.com>
2023-01-23 09:02:44 -0800

    Code does not access the data. Moving on to a different way

--- Java/AccessData/Data.py
+++ Java/AccessData/Data.py
@@ -16,14 +16,18 @@ shootingPercentage = []  # Shooting percentrage of each team
 driver.get("https://octane.gg/stats/teams?mode=3&minGames=50&group=rlcs2122fall")
 
 content = driver.page_source
+num = 1
 soup = BeautifulSoup('features = "lxml"')
-for a in soup.findAll('div', href=True, attrs={'class': 'chakra-stack css-8g1510'}):
-    name = a.find('a', attrs={'class': 'chakra-link css-1r88v6v'})
-    image = a.find('img', attrs={'class': 'chakra-image css-10xqgwl'})
-    teamName.append(name.text)
-    teamImage.append(image.text)
+for a in soup.findAll('div', attrs={'class': 'css-8d3ssv'}):
+    # name = a.find('div', attrs={'class': 'chakra-link css-1r88v6v'})
+    # image = a.find('img', attrs={'class': 'chakra-image css-10xqgwl'})
+    # teamName.append(name.text)
+    # teamImage.append(image.text)
+    teamName.append(num)
+    teamImage.append(num)
+    num += 1
 
-teamName.append(1)
-teamImage.append(1)
+teamName.append("hi")
+teamImage.append("hi")
 df = pd.DataFrame({'Team Name:': teamName, 'Image:': teamImage})
 df.to_csv('Teams.csv', index=False, encoding='utf-8')
