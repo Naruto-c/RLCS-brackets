6ad027c084b027ab6a7fa6af6b3d33d31afce0c0
Ian lai <Leadmlg.wolfy@gmail.com>
2023-01-25 13:43:11 -0800

    Import more machine learning libraries
    Still figuring out how to get it to work

--- Python/Machine Learning/machineTraining.py
+++ Python/Machine Learning/machineTraining.py
@@ -1,10 +1,30 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from category_encoders import OrdinalEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score
 
-
+le = preprocessing.LabelEncoder()
 forest_clf = RandomForestClassifier(random_state=22)
-RLCS_Data = pd.read_csv("/Desktop/Learning/Teams.csv")
+RLCS_Data = pd.read_csv(
+    "/Users/ianlai/Downloads/Repo gitkraken/RLCS-Brackets/Python/AccessData/Teams.csv")
 
-y_gt0 = (y > 0)
-cross_val_score(forest_clf, XTrain, y_gt0, cv=3, scoring="accuracy")
+model = make_pipeline(
+    OrdinalEncoder(),
+    SimpleImputer(),
+    RandomForestClassifier()
+)
+X = RLCS_Data['Played']
+y = RLCS_Data['Win %']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=2)
+model.fit(X_train, y_train)
+print('THE BASELINE ACCURACY IS: {}'.format(
+    y_train.value_counts(normalize=True).max()))
+
+x = accuracy_score(model.predict(X_test), y_test)
+
+print('FOREST MODEL ACCURACY: {}'.format(x))
